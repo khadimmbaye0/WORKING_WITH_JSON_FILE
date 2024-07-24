6b771cdcf1805d9c210280c01d07b3b2a66aa512
Khadim Mbaye <khadimmbaye4002@gmail.com>
2024-07-24 22:57:45 +0000

    try catch bloc added

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 const displayBooks = [];
 
 function fetchBooks() {
-    fetch('file.json') 
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(book => {
-            console.log('List of Books:');
-            console.log(`Title: ${book.title}`);
-            console.log(`Author: ${book.author}`);
-            console.log(`Publication Date: ${book.publicationDate}`);
-            console.log("==========================================");
-            displayBooks.push(`<p>Title: ${book.title}</p>
-                 <p>Author: ${book.author}</p>
-                 <p>Publication Date: ${book.publicationDate}</p><hr>`);
-        });
-        document.getElementById('bookItem').innerHTML = displayBooks.join('');
+    try {
+        fetch('file.json') 
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(book => {
+                console.log('List of Books:');
+                console.log(`Title: ${book.title}`);
+                console.log(`Author: ${book.author}`);
+                console.log(`Publication Date: ${book.publicationDate}`);
+                console.log("==========================================");
+                displayBooks.push(`<p>Title: ${book.title}</p>
+                    <p>Author: ${book.author}</p>
+                    <p>Publication Date: ${book.publicationDate}</p><hr>`);
+            });
+            document.getElementById('bookItem').innerHTML = displayBooks.join('');
     })
+    } catch (error) {
+        console.log("Erreur sur la recuperation des donnees JSON: ",error);
+    }
 }
 
 fetchBooks();
